18e8d945911b2d2bdb488114c33cbdc7821e6fb0
Daniel <estevesd8@gmail.com>
2020-05-06 14:16:14 -0400

    Code example as in the course

--- functions/generate-opengraph/generate-opengraph.js
+++ functions/generate-opengraph/generate-opengraph.js
@@ -4,34 +4,25 @@ exports.handler = async function (event, ctx) {
   const browser = await playwright.launchChromium();
   const context = await browser._defaultContext;
   const page = await context.newPage();
-
-  await page.setContent(`
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Dynamic OpenGraph Images</title>
-      </head>
-      <body>
-        <div id="app">
-          <div>Holaaaaa</div>
-        </div>
-      </body>
-    </html>
+  await page.setContent(`<!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8" />
+    </head>
+  
+    <body>
+      <div id="corgi"><div>CORGIIIS</div></div>
+    </body>
+  </html>
   `);
-
-  const boundingReact = await page.evaluate(() => {
-    const app = document.getElementById("app");
-    const { x, y, width, height } = app.children[0].getBoundingClientRect();
-
+  const boundingRect = await page.evaluate(() => {
+    const corgi = document.getElementById("corgi");
+    const { x, y, width, height } = corgi.children[0].getBoundingClientRect();
     return { x, y, width, height };
   });
 
-  const screenshotBuffer = await page.screenshot({ clip: boundingReact });
-
+  const screenshotBuffer = await page.screenshot({ clip: boundingRect });
   await browser.close();
-
   return {
     isBase64Encoded: true,
     statusCode: 200,
